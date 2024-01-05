72c620487f2ebb095490c8ed733b5552963f9929
maikeljh <mjh191003@gmail.com>
2024-01-05 22:29:16 +0700

    feat: setup dev environment

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
         <br />
         <Box>
           <h1 className="flex items-center justify-center mt-10 italic mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white ">
-            NextJS + Azle Canister Template
+            NextJS + Azle Canister
           </h1>
         </Box>
         <br />
