35c255525f8aae391f8876a5498afc5e113d65e1
Kenneth Dave <dave.bahana@gmail.com>
2024-01-06 20:11:48 +0700

    feat: adding first modal

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { makeAzleActor } from "../service/actor";
 import { Box, Button, Center } from "@chakra-ui/react";
 import { _SERVICE as AZLE } from "@/config/declarations/dfx_generated/azle.did";
+import Modal1 from "../components/modals/FailedToJoinLottery";
 
 export default function Home() {
   const [message, setMessage] = useState<number>();
@@ -22,5 +23,6 @@ export default function Home() {
       setLoading(false);
     }
   };
-  return <div>Home</div>;
+  // return <div>Home</div>;
+  return <Modal1></Modal1>;
 }
---
+++ components/modals/FailedToJoinLottery.tsx
@@ -0,0 +1,52 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    Button,
+    Heading,
+    Flex,
+} from '@chakra-ui/react'
+import Failed from "../../public/assets/failed.png";
+import Image from "next/image";
+const FailedToJoinLottery = () => {
+const { isOpen, onOpen, onClose } = useDisclosure()
+
+return (
+    <>
+        <Button onClick={onOpen}>Open Modal</Button>
+
+        <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent display="flex" alignItems="center" justifyContent="center">
+                <ModalHeader pb={6} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                    <Flex>
+                        <Image src={Failed} alt="logo" className="w-[9rem]" />
+                    </Flex>
+                </ModalHeader>
+                <ModalCloseButton />
+                <ModalBody pb={6} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                    <Heading as="h5" size="xl">
+                        Lottery failed to join!
+                    </Heading>
+                </ModalBody>
+
+                <ModalFooter pb={6} display="flex" alignItems="center" justifyContent="center">
+                <Button className='!bg-danger'  color="white" mr={15} onClick={onClose}>
+                    TRY AGAIN
+                </Button>
+                <Button className='!text-primary-3-500 !bg-white !outline-primary-3-500'ml={15} onClick={onClose}>
+                    BACK TO HOME
+                </Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    </>
+)
+}
+
+export default FailedToJoinLottery;
