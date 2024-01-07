1860d31c155ab725f3f4c18b1871fd6c4c3b8f42
maikeljh <mjh191003@gmail.com>
2024-01-07 11:43:03 +0700

    feat: add create lottery

--- app/(lottery)/lotteries/create/page.tsx
+++ app/(lottery)/lotteries/create/page.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import Card from "@/components/common/Card";
 import Banner from "../../../../public/assets/banner.png";
-import { Flex } from "@chakra-ui/react";
-import { useState } from "react";
+import { Button, Flex } from "@chakra-ui/react";
+import { ChangeEvent, useState } from "react";
+import Add from "../../../../public/assets/add.png";
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
 
 const Page = () => {
   const [phase, setPhase] = useState("pick");
@@ -38,6 +42,31 @@ const Page = () => {
       ),
     },
   ];
+
+  const [prize, setPrize] = useState([
+    {
+      rank: 1,
+      prize: "",
+    },
+  ]);
+
+  const [imageSrc, setImageSrc] = useState<string | StaticImageData>(Add);
+  const [file, setFile] = useState<File>();
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    setFile(file);
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        setImageSrc(reader.result as string);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <>
       {phase === "pick" ? (
@@ -67,9 +96,149 @@ const Page = () => {
       ) : (
         <Flex direction={"column"} gap={"2rem"}>
           <h1 className="text-3xl font-bold">Create a Lottery</h1>
-          <Flex direction={"row"} gap={"2rem"} wrap={"wrap"}>
-            <Flex></Flex>
-            <Flex></Flex>
+          <Flex
+            direction={"row"}
+            gap={"2rem"}
+            bg={"white"}
+            padding={"2rem"}
+            shadow={"xl"}
+            rounded={"lg"}
+          >
+            <Flex direction={"column"} gap={"2rem"} width={"50%"}>
+              <Flex gap={"1rem"} align={"center"}>
+                <h1 className="text-2xl font-bold min-w-[12rem]">
+                  Lottery Name<span className="text-danger">*</span>
+                </h1>
+                <input
+                  type="text"
+                  required
+                  className="border-2 rounded-lg w-full p-2"
+                />
+              </Flex>
+              <div className="w-full">
+                <label htmlFor="file-input" className="cursor-pointer">
+                  {typeof imageSrc === "string" ? (
+                    <img
+                      src={imageSrc}
+                      alt="Add thumbnail"
+                      className="max-h-[200px] mx-auto"
+                    />
+                  ) : (
+                    <Image
+                      src={imageSrc}
+                      alt="Add thumbnail"
+                      className="w-full"
+                    />
+                  )}
+                </label>
+                <input
+                  type="file"
+                  className="hidden"
+                  id="file-input"
+                  onChange={handleFileChange}
+                />
+              </div>
+              <Flex direction={"column"} gap={"1rem"}>
+                <h1 className="text-2xl font-bold min-w-[12rem]">
+                  Lottery Description<span className="text-danger">*</span>
+                </h1>
+                <textarea className="w-full min-h-[8rem] border-2 p-2 rounded-lg" />
+              </Flex>
+              {phase === "group" ? (
+                <Flex gap={"1rem"} align={"center"}>
+                  <Flex direction={"column"} gap={"1rem"} w={"50%"}>
+                    <h1 className="text-2xl font-bold min-w-[12rem]">
+                      Lottery End Date<span className="text-danger">*</span>
+                    </h1>
+                    <input type="date" className="p-2 border-2 rounded-lg" />
+                  </Flex>
+                  <Flex direction={"column"} gap={"1rem"} w={"50%"}>
+                    <h1 className="text-2xl font-bold min-w-[12rem]">
+                      Lottery Group<span className="text-danger">*</span>
+                    </h1>
+                    <select className="p-2 border-2 rounded-lg">
+                      <option>Anjay</option>
+                    </select>
+                  </Flex>
+                </Flex>
+              ) : (
+                <Flex direction={"column"} gap={"1rem"}>
+                  <h1 className="text-2xl font-bold min-w-[12rem]">
+                    Lottery End Date<span className="text-danger">*</span>
+                  </h1>
+                  <input type="date" className="p-2 border-2 rounded-lg" />
+                </Flex>
+              )}
+            </Flex>
+            <Flex direction={"column"} gap={"2rem"} width={"50%"}>
+              <Flex gap={"1rem"} align={"center"}>
+                <h1 className="text-2xl font-bold min-w-[12rem]">
+                  Lottery Winner Prizes<span className="text-danger">*</span>
+                </h1>
+                <Link href="/lotteries/1" className="ml-auto">
+                  <Button
+                    width={"6rem"}
+                    fontSize={"small"}
+                    className="!bg-primary-1-400"
+                    color={"white"}
+                  >
+                    Save Lottery
+                  </Button>
+                </Link>
+              </Flex>
+              <Flex gap={"1rem"} align={"center"}>
+                <Flex gap={"1rem"} direction={"column"}>
+                  <h1 className="text-2xl font-bold min-w-[12rem]">Ranking</h1>
+                  {prize &&
+                    prize.map((prize, idx) => (
+                      <input
+                        type="number"
+                        className="w-full border-2 p-2"
+                        key={idx}
+                        value={prize.rank}
+                      />
+                    ))}
+                </Flex>
+                <Flex gap={"1rem"} direction={"column"} className="w-full">
+                  <h1 className="text-2xl font-bold min-w-[12rem]">Prize</h1>
+                  {prize &&
+                    prize.map((prize, idx) => (
+                      <input
+                        type="number"
+                        className="w-full border-2 p-2"
+                        key={idx}
+                        value={prize.prize}
+                      />
+                    ))}
+                </Flex>
+                <Flex gap={"1rem"} direction={"column"} align={"center"}>
+                  <h1 className="text-2xl font-bold min-w-[4rem] text-center">
+                    Add
+                  </h1>
+                  {prize &&
+                    prize.map((e, idx) => (
+                      <Button
+                        width={"2rem"}
+                        fontSize={"xl"}
+                        className="!bg-primary-1-400"
+                        color={"white"}
+                        key={idx}
+                        onClick={() =>
+                          setPrize([
+                            ...prize,
+                            {
+                              rank: 0,
+                              prize: "",
+                            },
+                          ])
+                        }
+                      >
+                        +
+                      </Button>
+                    ))}
+                </Flex>
+              </Flex>
+            </Flex>
           </Flex>
         </Flex>
       )}
