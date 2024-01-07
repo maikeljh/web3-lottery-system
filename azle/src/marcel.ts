889f83fd2fe84ccd5debe35b4924df8d9216c3ae
MarcelRyan <marcel.ryan2004@gmail.com>
2024-01-07 22:41:49 +0700

    feat: creategroup and detail group (backend not yet tested)

--- azle/src/marcel.ts
+++ azle/src/marcel.ts
@@ -51,7 +51,7 @@ type PrizePayload = typeof PrizePayload.tsType
 
 const LotteryDAO = Record({
     id: Principal,
-    type: text,
+    types: text,
     title: text,
     description: text,
     prizes: Vec(PrizeDAO),
@@ -67,7 +67,7 @@ const LotteryDAO = Record({
 type LotteryDAO = typeof LotteryDAO.tsType
 
 const LotteryPayload = Record({
-    type: text,
+    types: text,
     title: text,
     description: text,
     prizes: Vec(PrizePayload),
@@ -80,7 +80,7 @@ type LotteryPayload = typeof LotteryPayload.tsType
 
 const LotteryDetailPayload = Record({
     id: Principal,
-    type: text,
+    types: text,
     title: text,
     description: text,
     prizes: Vec(PrizeDAO),
@@ -95,6 +95,69 @@ const LotteryDetailPayload = Record({
 })
 type LotteryDetailPayload = typeof LotteryDetailPayload.tsType
 
+const Roles = Record({
+    id: Principal,
+    groupId: Principal,
+    name: text,
+    rank: int
+})
+type Roles = typeof Roles.tsType
+
+const RolesPayload = Record({
+    name: text,
+    rank: int
+})
+type RolesPayload = typeof RolesPayload.tsType
+
+const UserRole = Record({
+    userId: Principal,
+    groupId: Principal,
+    username: text,
+    userRole: text
+})
+type UserRole = typeof UserRole.tsType
+
+
+/* Oke jadi intinya si UserGroup ini gua pake buat jadi key stablebtreemap userroles
+nah jadi kalau misal nanti mau bikin function yang nge assign role ke user di sebuah grup
+sebelum masukin ke userroles nanti bikin dulu variabel UserGroup buat jadi keynya biar bisa dimasukin
+ */
+const UserGroup = Record({
+    userId: Principal,
+    groupId: Principal,
+})
+type UserGroup = typeof UserGroup.tsType
+
+const GroupDAO = Record({
+    id: Principal,
+    name: text,
+    groupLotteries: Vec(Principal),
+    avatar: blob,
+    ownerId: Principal,
+    members: Vec(Principal),
+    roles: Vec(Roles)
+})
+type GroupDAO = typeof GroupDAO.tsType
+
+const GroupPayload = Record({
+    name: text,
+    roles: Vec(RolesPayload),
+    avatar: blob,
+    ownerId: Principal
+})
+type GroupPayload = typeof GroupPayload.tsType
+
+const DetailGroupPayload = Record({
+    id: Principal,
+    name: text,
+    groupLotteries: Vec(LotteryPayload),
+    avatar: blob,
+    ownerId: Principal,
+    members: Vec(UserRole),
+    roles: Vec(Roles)
+})
+type DetailGroupPayload = typeof DetailGroupPayload.tsType
+
 const Error = Variant({
     InvalidPayload: text,
     InvalidId: text,
@@ -104,12 +167,15 @@ const Error = Variant({
 let users = StableBTreeMap<Principal, User>(0);
 let prizes = StableBTreeMap<Principal, PrizeDAO>(1);
 let lotteries = StableBTreeMap<Principal, LotteryDAO>(2);
+let roles = StableBTreeMap<Principal, RolesPayload>(3);
+let groups = StableBTreeMap<Principal, GroupDAO>(4);
+let userroles = StableBTreeMap<UserGroup, UserRole>(5);
 
 export default Canister({
     createLottery: update([LotteryPayload], Result(LotteryDAO, Error), (payload) => {
         try {
             // Validating payload attributes
-            if (!payload.type || !payload.title || !payload.description || !payload.prizes || !payload.hostId || !payload.createdAt || !payload.endedAt || !payload.lotteryBanner) {
+            if (!payload.types || !payload.title || !payload.description || !payload.prizes || !payload.hostId || !payload.createdAt || !payload.endedAt || !payload.lotteryBanner) {
                 return Err({ InvalidPayload: `Payload is not valid!`})
             }
             
@@ -145,7 +211,7 @@ export default Canister({
                 participantsAmount: 0n,
                 winners: [],
                 isCompleted: false,
-                type: payload.type,
+                types: payload.types,
                 description: payload.description,
                 title: payload.title,
                 prizes: lotteryPrizes,
@@ -187,7 +253,7 @@ export default Canister({
                 
                 if ("None" in userOpt) {
                     return Err({
-                        InvalidId: `Lottery with that id doesn't exist`
+                        InvalidId: `User with that id doesn't exist`
                     })
                 }
 
@@ -198,7 +264,7 @@ export default Canister({
 
             const detailLottery: LotteryDetailPayload = {
                 id: lottery.id,
-                type: lottery.type,
+                types: lottery.types,
                 title: lottery.title,
                 description: lottery.description,
                 prizes: lottery.prizes,
@@ -218,7 +284,133 @@ export default Canister({
                 Fail : `Failed to get lottery detail: ${error}`
             })
         }
+    }),
+
+    createGroup: update([GroupPayload], Result(GroupDAO, Error), (payload) => {
+        try {
+            if (!payload.avatar || !payload.name || !payload.roles || !payload.ownerId) {
+                return Err({
+                    InvalidPayload: `Payload is not valid!`
+                })
+            }
+
+            let groupId = generateId()
+
+            while(!("None" in groups.get(groupId))) {
+                groupId = generateId()
+            }
+
+            let groupRoles: Roles[] = []
+
+            payload.roles.forEach((groupRole) => {
+                let roleId = generateId()
+
+                while(!("None" in groups.get(roleId))) {
+                    roleId = generateId()
+                }
+                const role = {
+                    id: roleId,
+                    groupId: groupId,
+                    ...groupRole
+                }
+
+                roles.insert(role.id, role)
+
+                groupRoles = [...groupRoles, role]
+            })
+
+            const group = {
+                id: groupId,
+                name: payload.name,
+                groupLotteries: [],
+                avatar: payload.avatar,
+                ownerId: payload.ownerId,
+                members: [],
+                roles: groupRoles
+            }
+
+            groups.insert(group.id, group)
+
+            return Ok(group);
+        } catch(error: any) {
+            return Err({
+                Fail: `Failed creatin group: ${error}`
+            })
+        }
+    }),
+
+    detailGroup: query([Principal], Result(DetailGroupPayload, Error), (groupId) => {
+        try {
+            // Validating group id
+            if (!groupId) {
+                return Err({ InvalidPayload: `Id is not valid!` });
+            }
+
+            let groupMembers: UserRole[] = [];
+
+            let groupLotteries: LotteryPayload[] = [];
+
+            const groupOpt = groups.get(groupId)
+
+            if ("None" in groupOpt) {
+                return Err({
+                    InvalidId: `Group with that id doesn't exist`
+                })
+            }
+
+            const group = groupOpt.Some
+
+            group.groupLotteries.forEach((lotteryId) => {
+                const lotteryOpt = lotteries.get(lotteryId)
+
+                if ("None" in lotteryOpt) {
+                    return Err({
+                        InvalidId: `Lottery with that id doesn't exist`
+                    })
+                }
+
+                let lottery = lotteryOpt.Some
+
+                groupLotteries = [...groupLotteries, lottery]
+            })
+
+            group.members.forEach((userId) => {
+                const userGroup: UserGroup = {
+                    userId: userId,
+                    groupId: group.id
+                }
+
+                const userroleOpt = userroles.get(userGroup)
+
+                if ("None" in userroleOpt) {
+                    return Err({
+                        InvalidId: `Information of user's role doesn't exist`
+                    })
+                }
+
+                let userrole = userroleOpt.Some
+
+                groupMembers = [...groupMembers, userrole]
+            })
+
+            const detailgroup: DetailGroupPayload = {
+                id: group.id,
+                name: group.name,
+                groupLotteries: groupLotteries,
+                avatar: group.avatar,
+                ownerId: group.ownerId,
+                members: groupMembers,
+                roles: group.roles
+            }
+
+            return Ok(detailgroup);
+        } catch (error:any) {
+            return Err({
+                Fail : `Failed to get group detail: ${error}`
+            })
+        }
     })
+
 })
 
 function generateId(): Principal {
