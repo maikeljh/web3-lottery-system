06ee4bedf91c71b6645f42c91ed057a644f2cf7c
maikeljh <mjh191003@gmail.com>
2024-01-08 12:09:54 +0700

    feat: update detail completed and notify lottery

--- app/(lottery)/lotteries/[slug]/page.tsx
+++ app/(lottery)/lotteries/[slug]/page.tsx
@@ -239,7 +239,205 @@ const Page = () => {
           )}
         </Flex>
       ) : condition === "completed" ? (
-        <></>
+        <>
+          <Flex padding={"2rem"} direction={"column"} gap={"3rem"}>
+            <Flex direction={"row"} gap={"5rem"}>
+              <Flex direction={"column"} gap={"2rem"} w={"60%"}>
+                <Flex align="center">
+                  <Image src={Thumbnail} alt="thumbnail" className="w-full" />
+                </Flex>
+                <Flex align="center" direction={"row"} gap={"1rem"} mx={"auto"}>
+                  <Flex align="center">
+                    <Image src={Users} alt="users" className="w-full" />
+                  </Flex>
+                  <Flex align="center" direction={"column"}>
+                    <p className="text-xl font-bold">30,000 Participants</p>
+                  </Flex>
+                </Flex>
+                <Flex direction={"column"} gap={"0.5rem"}>
+                  <h1 className="text-2xl font-bold">Description</h1>
+                  <p className="p-4 bg-white shadow-xl rounded-lg">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Eros in cursus turpis massa tincidunt dui.
+                    Scelerisque in dictum non consectetur a. Id eu nisl nunc mi
+                    ipsum faucibus vitae aliquet nec. Cras adipiscing enim eu
+                    turpis egestas .
+                  </p>
+                </Flex>
+              </Flex>
+              <Flex direction={"column"} w={"40%"} gap={"2rem"}>
+                <Flex direction={"column"} gap={"1rem"}>
+                  <h1 className="text-3xl font-bold">Lottery Name</h1>
+                  <Flex
+                    gap={"1rem"}
+                    align={"center"}
+                    className="border-t-[1px] border-b-[1px] border-black"
+                    padding={"1rem"}
+                  >
+                    <h1 className="text-2xl font-bold">Host</h1>
+                    <Flex
+                      align="center"
+                      className="border-2 border-primary-2-400 rounded-full"
+                    >
+                      <Image
+                        src={Leader}
+                        alt="user"
+                        className="w-[3rem] h-[3rem]"
+                      />
+                    </Flex>
+                    <Flex direction={"column"} fontWeight={"semibold"}>
+                      <p>Sebastian</p>
+                      <p>@username</p>
+                    </Flex>
+                  </Flex>
+                  <Flex align={"center"} gap={"2rem"} fontWeight={"semibold"}>
+                    <Flex align="center">
+                      <Image src={Calendar} alt="calendar" className="w-full" />
+                    </Flex>
+                    <Flex align="center" direction={"column"}>
+                      <p>Created at : 10/12/23</p>
+                      <p>End at : 10/12/23</p>
+                    </Flex>
+                  </Flex>
+                </Flex>
+                <Flex direction={"column"} gap={"0.5rem"}>
+                  <h1 className="text-2xl font-bold">{"Winner's Prizes"}</h1>
+                  <Flex gap={"1rem"} align={"center"}>
+                    <div className="rounded-[50%] bg-primary-2-400 w-12 h-12 flex items-center justify-center text-white text-xl">
+                      1
+                    </div>
+                    <h2 className="text-xl font-bold">100 BTC</h2>
+                  </Flex>
+                  <Flex gap={"1rem"} align={"center"}>
+                    <div className="rounded-[50%] bg-primary-2-400 w-12 h-12 flex items-center justify-center text-white text-xl">
+                      2
+                    </div>
+                    <h2 className="text-xl font-bold">50 BTC</h2>
+                  </Flex>
+                  <Flex gap={"1rem"} align={"center"}>
+                    <div className="rounded-[50%] bg-primary-2-400 w-12 h-12 flex items-center justify-center text-white text-xl">
+                      3
+                    </div>
+                    <h2 className="text-xl font-bold">20 BTC</h2>
+                  </Flex>
+                  <Flex gap={"1rem"} align={"center"}>
+                    <div className="rounded-[50%] bg-primary-2-400 w-12 h-12 flex items-center justify-center text-white text-xl">
+                      4-10
+                    </div>
+                    <h2 className="text-xl font-bold">10 BTC</h2>
+                  </Flex>
+                </Flex>
+              </Flex>
+            </Flex>
+            <Flex direction={"column"} gap={"1rem"}>
+              <Flex>
+                <h1 className="text-3xl font-bold">Winners</h1>
+              </Flex>
+              <Flex direction={"column"} gap={"1rem"}>
+                {data &&
+                  data.map((user, index) => (
+                    <>
+                      <Flex
+                        bg="white"
+                        padding={"1rem"}
+                        className="shadow-lg"
+                        gap={"1rem"}
+                        align={"center"}
+                      >
+                        <Flex
+                          align="center"
+                          className="border-2 border-primary-2-400 rounded-full"
+                        >
+                          <Image
+                            src={Leader}
+                            alt="user"
+                            className="w-[1.75rem]"
+                          />
+                        </Flex>
+                        <Flex direction={"column"} fontWeight={"semibold"}>
+                          <p>{user.name}</p>
+                          <p>@{user.username}</p>
+                        </Flex>
+                        <Spacer />
+                        <p className="font-semibold">Iphone 15</p>
+                      </Flex>
+                    </>
+                  ))}
+              </Flex>
+            </Flex>
+            <Flex direction={"column"} gap={"1rem"}>
+              <Flex>
+                <h1 className="text-3xl font-bold">Participants</h1>
+                <Spacer />
+                <Link
+                  href="/lotteries/1/participants"
+                  className="underline font-semibold"
+                >
+                  See all
+                </Link>
+              </Flex>
+              <Flex direction={"column"} gap={"1rem"}>
+                {data &&
+                  data.map((user, index) => (
+                    <>
+                      <Flex
+                        bg="white"
+                        padding={"1rem"}
+                        className="shadow-lg"
+                        gap={"1rem"}
+                        align={"center"}
+                      >
+                        <Flex
+                          align="center"
+                          className="border-2 border-primary-2-400 rounded-full"
+                        >
+                          <Image
+                            src={Leader}
+                            alt="user"
+                            className="w-[1.75rem]"
+                          />
+                        </Flex>
+                        <Flex direction={"column"} fontWeight={"semibold"}>
+                          <p>{user.name}</p>
+                          <p>@{user.username}</p>
+                        </Flex>
+                        <Spacer />
+                        <p className="font-semibold">{user.joined}</p>
+                      </Flex>
+                    </>
+                  ))}
+              </Flex>
+            </Flex>
+          </Flex>
+        </>
+      ) : condition === "notify" ? (
+        <>
+          <Flex direction={"column"} gap={"1rem"}>
+            <h1 className="text-3xl font-bold text-center">@username</h1>
+            <Flex
+              align="center"
+              className="border-3 border-primary-2-400 rounded-full w-full mx-auto"
+              direction={"column"}
+            >
+              <Image
+                src={Leader}
+                alt="leader"
+                className="w-[12rem] h-[12rem]"
+              />
+            </Flex>
+            <h1 className="text-5xl font-bold text-center">CONGRATULATIONS!</h1>
+            <Flex bg={"white"} padding={"1rem"} shadow={"xl"} rounded={"lg"}>
+              <p className="text-2xl font-semibold text-black">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Eros
+                in cursus turpis massa tincidunt dui. Scelerisque in dictum non
+                consectetur a. Id eu nisl nunc mi ipsum faucibus vitae aliquet
+                nec. Cras adipiscing enim eu turpis egestas .
+              </p>
+            </Flex>
+          </Flex>
+        </>
       ) : (
         <></>
       )}
